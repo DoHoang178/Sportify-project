8f405cf3797eccdd9e4f98b9e39a198a2f8f9ced
Hoang <dohoang1782000@gmail.com>
2022-05-07 13:16:18 +0700

    start

---
+++ sportify-project/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App bg-slate-400">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
